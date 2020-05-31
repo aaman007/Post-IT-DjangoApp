62213a6b79fc7632e284092e620574fbdf546b89
aaman007 <aaman.sunny007@gmail.com>
2020-06-01 00:16:13 +0600

    Templates integrated with dummy data for posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author' : 'aaman007',
+        'title' : 'Abracadabra',
+        'content' : 'This is a magical post',
+        'date_posted' : '31st May, 2020'
+    },
+    {
+        'author' : 'Decayed',
+        'title' : 'Abracadabra2',
+        'content' : 'This is also a magical post',
+        'date_posted' : '1st June, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts' : posts,
+        'title' : 'Home'
+    }
+    return render(request, "blog/home.html", context)
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html", { 'title' : 'About' })
