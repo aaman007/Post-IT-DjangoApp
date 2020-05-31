713005a202d93ee1822109eba3270ed4e92c8ced
aaman007 <aaman.sunny007@gmail.com>
2020-06-01 01:30:24 +0600

    Post model added with templates having dynamic contents

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
 
-posts = [
-    {
-        'author' : 'aaman007',
-        'title' : 'Abracadabra',
-        'content' : 'This is a magical post',
-        'date_posted' : '31st May, 2020'
-    },
-    {
-        'author' : 'Decayed',
-        'title' : 'Abracadabra2',
-        'content' : 'This is also a magical post',
-        'date_posted' : '1st June, 2020'
-    }
-]
-
 def home(request):
+    posts = Post.objects.all().order_by('-date_published')
     context = {
         'posts' : posts,
         'title' : 'Home'
