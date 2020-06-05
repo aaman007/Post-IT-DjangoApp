5f4bd781566e608ecd6494eddbef0c637098789f
aaman007 <aaman.sunny007@gmail.com>
2020-06-05 20:12:15 +0600

    Fix : Registration is now allowed without tokens

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from .serializers import RegistrationSerializer, AccountPropertiesSerializer
 from rest_framework.authtoken.models import Token
@@ -10,6 +10,7 @@ from django.contrib.auth.models import User
 
 
 @api_view(['POST', ])
+@permission_classes((AllowAny, ))
 def registration_view(request):
     serializer = RegistrationSerializer(data=request.data)
     data = {}
