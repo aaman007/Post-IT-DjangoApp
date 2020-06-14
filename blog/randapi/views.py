3bbfb484050d0af5f360dc74e8cfc3806ff13c78
aaman007 <aaman.sunny007@gmail.com>
2020-06-14 19:58:25 +0600

    PostManager added to fetch random posts

--- blog/randapi/views.py
+++ blog/randapi/views.py
@@ -13,32 +13,7 @@ class NCategoryListAPIView(ListAPIView):
         category = self.kwargs.get('category')
         quantity = self.kwargs.get('quantity')
 
-        #posts = self.get_random_unused_posts(category, quantity)
-        #extra_posts = self.get_random_used_posts(category, quantity-posts.count())
-
         posts = Post.objects.get_random_unused_posts(category=category, quantity=quantity)
         extra_posts = Post.objects.get_random_used_posts(category=category, quantity=quantity-posts.count())
 
         return posts | extra_posts
-
-
-    def get_random_unused_posts(self, category, quantity):
-        posts = Post.objects.filter(category__icontains=category, status=False)
-        post_ids = []
-        for post in posts:
-            post_ids.append(post.id)
-        shuffle(post_ids)
-        post_ids = post_ids[:quantity]
-        posts = Post.objects.filter(id__in=post_ids)
-        return posts
-
-
-    def get_random_used_posts(self, category, quantity):
-        posts = Post.objects.filter(category__icontains=category, status=True)
-        post_ids = []
-        for post in posts:
-            post_ids.append(post.id)
-        shuffle(post_ids)
-        post_ids = post_ids[:quantity]
-        posts = Post.objects.filter(id__in=post_ids)
-        return posts
